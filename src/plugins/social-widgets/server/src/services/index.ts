e8d9e135575deb9a65a74a619bbed62570cf0030
Robert Castro <oneroberth@gmail.com>
2025-06-19 16:18:08 -0500

    Add fake data generation for Instagram and TikTok posts; update service index to include new fake-data service.

--- src/plugins/social-widgets/server/src/services/index.ts
+++ src/plugins/social-widgets/server/src/services/index.ts
@@ -1,5 +1,9 @@
+import service from './service';
 import instagram from './instagram';
+import fakeData from './fake-data';
 
 export default {
+  service,
   instagram,
+  'fake-data': fakeData,
 };
